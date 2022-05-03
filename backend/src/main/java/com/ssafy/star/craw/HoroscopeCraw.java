c9ff5c51d0a10ce14e21e341ca45e6c951ae7737
zlflshdh84 <zlflshdh84@gmail.com>
2022-05-03 11:51:41 +0900

    Feat : get NASA APOD URL and detach APIKEY to properties
    Former-commit-id: 6bba6aff9512b80811ae3e313be889ac1c9618b2

--- backend/src/main/java/com/ssafy/star/craw/HoroscopeCraw.java
+++ backend/src/main/java/com/ssafy/star/craw/HoroscopeCraw.java
@@ -1,25 +1,47 @@
 package com.ssafy.star.craw;
 
+import com.ssafy.star.common.ApiKey;
+import com.ssafy.star.db.dto.NasaDto;
 import com.ssafy.star.db.entity.Horoscope;
 import com.ssafy.star.db.repository.HoroscopeRepository;
+import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
-@EnableScheduling
-@Component
-public class HoroscopeCraw {
 
-    @Autowired
-    HoroscopeRepository horoscopeRepository;
+@Component
+@EnableScheduling
+@RequiredArgsConstructor
+public class HoroscopeCraw {
+    private final HoroscopeRepository horoscopeRepository;
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final ApiKey apiKey;
+    private final String NASA_APOD_API = "https://api.nasa.gov/planetary/apod?api_key={apikey}";
 
     private static String NAVER_LUCK_URL = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=";
     private static final String[] STAR_CODES = {"물병자리","물고기자리","양자리","황소자리","쌍둥이자리","게자리","사자자리","처녀자리","천칭자리","전갈자리","사수자리","염소자리"};
+
+    @Scheduled(cron="0 30 0 * * ?", zone="Asia/Seoul")
+    public void getUrl() {
+        String serviceKey = apiKey.getApiKey();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        NasaDto nasaDto = restTemplate.exchange(NASA_APOD_API, HttpMethod.GET, entity, NasaDto.class, serviceKey).getBody();
+        // TODO: DTO를 통해 가져온 url을 가공
+    }
 
 //     초    |   분   |   시   |   일   |   월   |  요일  |   연도
 //     0~59 | 0~59 | 0~23 | 1~31 | 1~12  |   0~6  | 생략가능
@@ -42,6 +64,5 @@ public class HoroscopeCraw {
                 System.out.println("오류 발생");
             }
         }
-
     }
 }
