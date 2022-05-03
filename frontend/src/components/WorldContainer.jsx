2dbd98a76f758c8682b8e525200b069d06b611ea
uiyn <uiyn@naver.com>
2022-05-03 13:55:01 +0900

    Feat : add amongus asset & first person view
    Former-commit-id: 4bf72e9bfd647f86425faeebd9ecf1687e3f82f5

--- frontend/src/components/WorldContainer.jsx
+++ frontend/src/components/WorldContainer.jsx
@@ -2,13 +2,21 @@ import 'aframe';
 
 const WorldContainer = () => {
   return (
-    <a-scene>
-      <a-sky src="src/assets/bg-world.jpg" />
-      <a-gltf-model src="src/assets/ground-world.glb" />
-      <a-entity position="0 10 0">
-        <a-camera id="camera" />
-      </a-entity>
-    </a-scene>
+    <>
+      <a-scene>
+        <a-entity>
+          <a-sky src="src/assets/bg-world.jpg" />
+        </a-entity>
+        <a-entity position="0 -5 0">
+          <a-gltf-model src="src/assets/ground-world.glb" />
+        </a-entity>
+        <a-camera position="0 7 0">
+          <a-entity position="0 -5 0" rotation="0 180 0">
+            <a-gltf-model src="src/assets/amongUs.glb" />
+          </a-entity>
+        </a-camera>
+      </a-scene>
+    </>
   );
 };
 
