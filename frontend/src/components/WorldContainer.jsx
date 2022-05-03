6f2be1f3d7a9b09a1f2c806aaa1335b9296ede74
박해인 <haein.hannah.park@gmail.com>
2022-05-03 15:46:29 +0900

    Feat: world outerior asset satellite added
    Former-commit-id: 822fae28d0d6731e3de56e3a284cbefed6dc1d66

--- frontend/src/components/WorldContainer.jsx
+++ frontend/src/components/WorldContainer.jsx
@@ -9,6 +9,7 @@ const WorldContainer = () => {
         <a-asset-item id="observatory" src="src/assets/observatory.glb"></a-asset-item>
         <a-asset-item id="spaceship" src="src/assets/spaceship.glb"></a-asset-item>
         <a-asset-item id="science-museum" src="src/assets/science-museum.glb"></a-asset-item>
+        <a-asset-item id="satellite" src="src/assets/satellite.glb"></a-asset-item>
       </a-assets>
       <a-sky src="src/assets/bg-world.jpg" />
       <a-gltf-model src="src/assets/ground-world.glb" />
@@ -18,17 +19,20 @@ const WorldContainer = () => {
       <a-entity scale="10 10 10" position="-63.970 23.804 -116.629">
         <a-gltf-model src="#fountain" />
       </a-entity>
-      <a-entity position="77.600 20.323 -192.333" rotation="0 90 0">
+      <a-entity position="69.672 20.323 -192.333" rotation="0 90 0">
         <a-gltf-model src="#library" />
       </a-entity>
-      <a-entity scale="100 100 100" position="84.404 28.755 -89.771">
+      <a-entity scale="100 100 100" position="41.511 29.884 -56.011" rotation="0 90 0">
         <a-gltf-model src="#observatory" />
       </a-entity>
       <a-entity scale="100 100 100" position="-67.182 0 -262.203">
         <a-gltf-model src="#spaceship" />
       </a-entity>
-      <a-entity scale="1.5 1.5 1.5" position="-47.915 4.456 -45.260">
+      <a-entity scale="2 2 2" position="-51.368 4.456 -51.946">
         <a-gltf-model src="#science-museum" />
+      </a-entity>
+      <a-entity scale="100 100 100" position="-68.910 46.147 -61.601" rotation="0 55.643 0">
+        <a-gltf-model src="#satellite" />
       </a-entity>
     </a-scene>
   );
