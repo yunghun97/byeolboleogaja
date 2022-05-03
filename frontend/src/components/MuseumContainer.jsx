5b65313733f150e8c9cd483aa4d34f02d2aec27a
윤숙 <dbstnr2146@naver.com>
2022-05-03 17:35:17 +0900

    Feat: add 3d assets
    Former-commit-id: 7e1fa0ebbc3bd55e35c4f1d47489d999b36124b8

--- frontend/src/components/MuseumContainer.jsx
+++ frontend/src/components/MuseumContainer.jsx
@@ -1,28 +1,45 @@
 // @ts-nocheck
 import 'aframe';
 import 'aframe-extras';
-import { useEffect } from 'react';
+
+
+AFRAME.registerComponent('movement', {
+
+  init () {
+    // Do something when component first attached.    
+    var sceneEl = document.querySelector('a-scene');
+    var CameraEl =sceneEl.querySelector('a-camera');
+    var player = sceneEl.querySelector('#player');
+    
+    player.setAttribute('animation-mixer', {clip: 'base'});
+
+  },
+
+  
+});
+
 
 const MuseumContainer = () => {
 
-  
-
   return (
-       <a-scene>
-           <a-assets>
+      <a-scene>
+          <a-assets>
                <a-asset-item id="model" src="/src/assets/amongUs.glb"></a-asset-item>
-           </a-assets>
-    
-           <a-gltf-model   position="0 0 0" scale="1.8,2,1.8" src="src/assets/showroom.glb" />
-    
-            <a-camera position="0 1.5 4">
-                <a-entity  position="0 -1.1 -1" rotation="0 180 0" scale="0.8 0.8 0.8">
-                  <a-gltf-model animation-mixer="clip: walk" scale="0.2, 0.2  0.2" keydown id="player" src="#model"/>
-               </a-entity>
-               
-            </a-camera>
+          </a-assets>
          
-         </a-scene>
+          <a-text value="허블우주망원경" font="/src/assets/text/BMYEONSUNG_ttf-msdf.json" font-image="/src/assets/text/BMYEONSUNGttf.png" negate="false" scale="2 2 1"  position="2 1.5 0"></a-text>
+          <a-gltf-model position="0 0 0" scale="1.8,2,2" src="src/assets/showroom.glb" />
+          <a-gltf-model position="0 0.4 0"  src="src/assets/spaceLine.glb" />
+          <a-gltf-model position="-0.8 1 -0.8" rotation="90,0,0"  src="src/assets/hubble space telescope.glb" animation="property: rotation; to: 0 360 0; loop: true; dur: 10000" />
+        
+          <a-camera movement position="0 1.5 4">
+            <a-entity  position="0 -1.1 -1" rotation="0 180 0" scale="0.8 0.8 0.8">
+              <a-gltf-model  scale="0.2, 0.2  0.2"  id="player" src="#model"/>
+            </a-entity>
+               
+          </a-camera>
+         
+      </a-scene>
   
 
   );
