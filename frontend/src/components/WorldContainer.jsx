27715870cb7df77f7b53697697b85dba27d9b4df
박해인 <haein.hannah.park@gmail.com>
2022-05-04 16:12:40 +0900

    fix: move world outerior observatory asset position
    Former-commit-id: 081ee0962632c96cf64ddc1bf7722fb3fb1b8516

--- frontend/src/components/WorldContainer.jsx
+++ frontend/src/components/WorldContainer.jsx
@@ -16,7 +16,7 @@ const WorldContainer = () => {
         <a-sky src="src/assets/bg-world.jpg" />
         <a-gltf-model src="#ground" position="0 -5 0" rotation="0 -2 0" />
         <a-gltf-model src="#library" position="82.276 14.178 -204.775" rotation="0 90 0" />
-        <a-gltf-model src="#observatory" scale="120 120 120" position="41.511 25 -56.011" rotation="0 90 0" />
+        <a-gltf-model src="#observatory" scale="120 120 120" position="41.511 29.726 -56.011" rotation="0 90 0" />
         <a-gltf-model src="#spaceship" scale="100 100 100" position="-60.211 -1.030 -262.203" />
         <a-gltf-model src="#science-museum" scale="2.3 2.3 2.3" position="-54.671 0 -86.823" />
         <a-gltf-model src="#satellite" scale="100 100 100" position="-69.931 45.263 -98.965" rotation="0 55.643 0" />
