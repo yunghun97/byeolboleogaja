ef1517c3edbffb626bef377eeb7c606aed7d0fec
박해인 <haein.hannah.park@gmail.com>
2022-05-04 14:02:17 +0900

    Feat: add world outerior asset witch-house & delete other assets & delete animation code
    Former-commit-id: 072c83cc3daeba7698cf5cc70482714875d4bbae

--- frontend/src/components/WorldContainer.jsx
+++ frontend/src/components/WorldContainer.jsx
@@ -1,17 +1,4 @@
 import 'aframe';
-import 'aframe-extras';
-
-AFRAME.registerComponent('movement', {
-
-  init () {
-    // Do something when component first attached.    
-    const sceneEl = document.querySelector('a-scene');
-    const CameraEl =sceneEl.querySelector('a-camera');
-    const crystalBall = sceneEl.querySelector('#crystal-ball');
-    
-    crystalBall.setAttribute('animation-mixer');
-  },
-});
 
 const WorldContainer = () => {
   return (
@@ -25,16 +12,16 @@ const WorldContainer = () => {
           <a-asset-item id="spaceship" src="src/assets/spaceship.glb"></a-asset-item>
           <a-asset-item id="science-museum" src="src/assets/science-museum.glb"></a-asset-item>
           <a-asset-item id="satellite" src="src/assets/satellite.glb"></a-asset-item>
-          <a-asset-item id="crystal-ball" src="src/assets/crystal-ball.glb"></a-asset-item>
+          <a-asset-item id="witch-house" src="src/assets/witch-house.glb"></a-asset-item>
         </a-assets>
         <a-sky src="src/assets/bg-world.jpg" />
         <a-gltf-model src="#ground" position="0 -5 0" rotation="0 -2 0" />
-        <a-gltf-model src="#library" position="82.276 12.255 -204.775" rotation="0 90 0" />
+        <a-gltf-model src="#library" position="82.276 14.178 -204.775" rotation="0 90 0" />
         <a-gltf-model src="#observatory" scale="120 120 120" position="41.511 25 -56.011" rotation="0 90 0" />
         <a-gltf-model src="#spaceship" scale="100 100 100" position="-60.211 -1.030 -262.203" />
         <a-gltf-model src="#science-museum" scale="2.3 2.3 2.3" position="-54.671 0 -86.823" />
         <a-gltf-model src="#satellite" scale="100 100 100" position="-69.931 45.263 -98.965" rotation="0 55.643 0" />
-        <a-gltf-model id="crystalBall" src="#crystal-ball" scale="0.1 0.1 0.1" position="89.062 9.391 -85.107" />
+        <a-gltf-model src="#witch-house" scale="0.3 0.3 0.3" position="-83.652 15.925 -359.571" rotation="179.947 0 -179.957"/>
         <a-camera position="0 7 0">
           <a-entity position="0 -5 0" rotation="0 180 0">
             <a-gltf-model src="src/assets/amongUs.glb" />
