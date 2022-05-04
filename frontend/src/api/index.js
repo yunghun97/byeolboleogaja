9b39c2c7ce4701b7457696e27d345a4d96a91824
오용록 <ohyr9685@gmail.com>
2022-05-04 17:48:37 +0900

    Fix : env setting in vite
    Former-commit-id: 35c6985a39f9fd6fe31af828c2c3c41cd1070908

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${import.meta.env.REACT_APP_API_URL}/api`,
+  baseURL: `${import.meta.env.VITE_APP_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
