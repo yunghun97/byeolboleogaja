2d02fc9c0bc78a4a8267fcc1d1373782b0fcfe44
윤숙 <dbstnr2146@naver.com>
2022-05-05 01:17:47 +0900

    Feat : Linking asset click
    Former-commit-id: c33b7dca17d4e93c7daf44dad8feb3b22c9055cd

--- frontend/src/components/MuseumContainer.jsx
+++ frontend/src/components/MuseumContainer.jsx
@@ -1,14 +1,33 @@
 // @ts-nocheck
 import 'aframe';
 import 'aframe-extras';
+import { useEffect } from 'react';
+
+
+AFRAME.registerComponent('clickhandler', {
+          schema: {
+            txt: {default:'default'}
+          },        
+          init: function () {
+            var data = this.data;
+            var el = this.el;        
+            el.addEventListener('click', function () {            
+            console.log(data.txt);
+            });        
+          }
+    });
+
+
+
 
 
 
 
 
 const MuseumContainer = () => {
-
+ 
   return (
+     
       <a-scene >
           <a-assets>
             <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
@@ -28,20 +47,22 @@ const MuseumContainer = () => {
           <a-gltf-model id="fence8" position="1 -1.3 10"  rotation="0 90 0"  src="src/assets/fence.glb" />
   
 
-          <a-text value="James Webb Space Telescope"  position="-15 1.5 -6"></a-text>
-          <a-text value="Hubble Space Telescope"   position="3 1.5 -6"></a-text>
-          <a-text value="Chandra X-ray Observatory"  position="25 1.5 -6"></a-text>
-          <a-text value="Spitzer Space Telescope"  position="15 2 10" rotation="0 180 0"></a-text>
-          <a-text value="Fermi Gamma-ray Large Area Space Telescope"  position="-3 2 10" rotation="0 180 0"></a-text>
-          <a-text value="Kepler space observatory"  position="-25 2 10" rotation="0 180 0"></a-text>
+          <a-text clickhandler="txt:james" value="James Webb Space Telescope"  position="-15 1.5 -6"></a-text>
+          <a-text clickhandler="txt:hubble" value="Hubble Space Telescope"   position="3 1.5 -6"></a-text>
+          <a-text clickhandler="txt:chandra" value="Chandra X-ray Observatory"  position="25 1.5 -6"></a-text>
+          <a-text clickhandler="txt:spitzer" value="Spitzer Space Telescope"  position="15 2 10" rotation="0 180 0"></a-text>
+          <a-text clickhandler="txt:fermi" value="Fermi Gamma-ray Large Area Space Telescope"  position="-3 2 10" rotation="0 180 0"></a-text>
+          <a-text clickhandler="txt:kepler" value="Kepler space observatory"  position="-25 2 10" rotation="0 180 0"></a-text>
 
-          <a-gltf-model id="james"  position="-19 3 -10" scale="0.5 0.5 0.5"  src="src/assets/james_webb_space_telescope.glb" />
-          <a-gltf-model id="hubble" position="-0.2 8 -12" rotation="-180 0 0"  src="src/assets/hubble space telescope.glb"  />
-          <a-gltf-model id="chandra" position="25 2 -10" rotation="0 90 10" src="src/assets/Chandra X-ray Observatory.glb"/>
-          <a-gltf-model id="spitzer" position="21 3 10" scale="0.6 0.6 0.6"  rotation="60 360 0" src="src/assets/Spitzer Space Telescope.glb"/>
-          <a-gltf-model id="fermi" position="1 3 10"  scale="0.8 0.8 0.8"  rotation="90 90 0" src="src/assets/Fermi Gamma-ray Large Area Space Telescope.glb"/>
-          <a-gltf-model id="kepler" position="-20 3 10"  rotation="0 90 0" src="src/assets/Kepler space observatory.glb"/>
-         
+          <a-gltf-model clickhandler="txt:james" id="james"  position="-19 3 -10" scale="0.5 0.5 0.5"  src="src/assets/james_webb_space_telescope.glb" />
+          <a-gltf-model clickhandler="txt:hubble" id="hubble" position="-0.2 8 -12" rotation="-180 0 0"  src="src/assets/hubble space telescope.glb"  />
+          <a-gltf-model clickhandler="txt:chandra" id="chandra" position="25 2 -10" rotation="0 90 10" src="src/assets/Chandra X-ray Observatory.glb"/>
+          <a-gltf-model clickhandler="txt:spitzer" id="spitzer" position="21 3 10" scale="0.6 0.6 0.6"  rotation="60 360 0" src="src/assets/Spitzer Space Telescope.glb"/>
+          <a-gltf-model clickhandler="txt:fermi" id="fermi" position="1 3 10"  scale="0.8 0.8 0.8"  rotation="90 90 0" src="src/assets/Fermi Gamma-ray Large Area Space Telescope.glb"/>
+          <a-gltf-model clickhandler="txt:kepler" id="kepler" position="-20 3 10"  rotation="0 90 0" src="src/assets/Kepler space observatory.glb"/>
+          
+          {/* <a-box clickhandler="txt:box1"></a-box> */}
+          <a-entity cursor="rayOrigin:mouse"></a-entity>
           <a-camera position="0 1.5 4">
             <a-entity position="0 -1.1 -1" rotation="0 180 0" scale="0.8 0.8 0.8">
               <a-gltf-model  scale="0.2 0.2 0.2"  id="player" src="#model"/>
