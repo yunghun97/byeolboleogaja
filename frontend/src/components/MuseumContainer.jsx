f8ae7fcbb299ff3161fbfd3892e0052fca0747ef
윤숙 <dbstnr2146@naver.com>
2022-05-06 09:52:57 +0900

    Refactor: 리팩토링 수정
    Former-commit-id: 7f296f09199bb7da80df7b03c4d7e50a4ed0b34c

--- frontend/src/components/MuseumContainer.jsx
+++ frontend/src/components/MuseumContainer.jsx
@@ -1,10 +1,37 @@
 
 import 'aframe';
 import 'aframe-extras';
-import { aframeMuseum } from '/src/components/museum.js';
 
 
-aframeMuseum()
+AFRAME.registerComponent('clickhandler', {
+  schema: {
+    txt: {default:'default'}
+  },        
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+    // function DetailItems() {
+
+    //   const [clicked, setClicked]= React.useState(false)
+  
+      
+  
+    //   return React.createElement(
+    //       'h1',
+    //       { onClick: () => setClicked(!clicked) },
+    //       'clicked',
+    //   );
+    // }        
+    el.addEventListener('click', function () {         
+           
+      console.log(data.txt)
+      const domContainer = document.getElementById('museum');
+      // ReactDOM.render(React.createElement(DetailItems), domContainer)
+
+    });        
+}
+})
+
 const MuseumContainer = () => {
  
   return (
