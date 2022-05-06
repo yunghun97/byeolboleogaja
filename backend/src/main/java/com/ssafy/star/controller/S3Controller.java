229c93d144c42e0b9c4ede3ac760210fc55bd4e5
zlflshdh84 <zlflshdh84@gmail.com>
2022-05-06 15:42:52 +0900

    Feat : add return url functions
    Former-commit-id: 33a807944e623c17ec15ec8dae1955c8bb5ca1bf

--- backend/src/main/java/com/ssafy/star/controller/S3Controller.java
+++ backend/src/main/java/com/ssafy/star/controller/S3Controller.java
@@ -1,15 +1,15 @@
 package com.ssafy.star.controller;
 
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.ssafy.star.common.ApiKey;
 import com.ssafy.star.common.S3Uploader;
-import com.ssafy.star.config.S3Config;
 import com.ssafy.star.db.dto.NasaDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -22,10 +22,13 @@ import java.io.File;
 @RequestMapping("/api/file")
 public class S3Controller {
     private final S3Uploader s3Uploader;
+    private final AmazonS3Client amazonS3Client;
 
     private final RestTemplate restTemplate = new RestTemplate();
     private final ApiKey apiKey;
     private final String NASA_APOD_API = "https://api.nasa.gov/planetary/apod?api_key={apikey}";
+
+    private String uploadURL;
 
     @Scheduled(cron="0 0 13 * * ?", zone="Asia/Seoul")
     public void getUrl() {
@@ -38,7 +41,17 @@ public class S3Controller {
         NasaDto nasaDto = restTemplate.exchange(NASA_APOD_API, HttpMethod.GET, entity, NasaDto.class, serviceKey).getBody();
 
         File nasaImg = s3Uploader.downloadImg(nasaDto.getHdurl(), nasaDto.getTitle());
-        s3Uploader.upload(nasaImg, "todayNASA");
+        uploadURL = s3Uploader.upload(nasaImg, "todayNASA");
+    }
+
+    @GetMapping
+    public String getImgUrl() {
+        return uploadURL;
+    }
+
+    @GetMapping("/asset/{fileName}")
+    public String getAssetUrl(@PathVariable String fileName) {
+        return amazonS3Client.getUrl(s3Uploader.bucket, fileName).toString();
     }
 }
 
