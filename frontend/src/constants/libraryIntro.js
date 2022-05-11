36e719c0f3bef447850af7ffbea20ebc583f6a20
uiyn <uiyn@naver.com>
2022-05-12 04:11:28 +0900

    Feat : add building intro contents
    Former-commit-id: 271c53dd6c5d4c471c1671754547fee99ff377bc

---
+++ frontend/src/constants/libraryIntro.js
@@ -0,0 +1,18 @@
+const libraryIntro = [
+  {
+    title: '도서관 NPC',
+    description:
+      '안녕하세요. 이곳은 천문학에 대한 정보를 볼 수 있는 도서관이에요.',
+  },
+  {
+    title: '도서관 NPC',
+    description:
+      '관심 있는 별이나 행성 등 관련된 책을 선택해 정보를 볼 수 있어요.',
+  },
+  {
+    title: '도서관 NPC',
+    description: '그럼, 도서관에 입장하시겠습니까?',
+  },
+];
+
+export default libraryIntro;
---
+++ frontend/src/constants/observatoryIntro.js
@@ -0,0 +1,17 @@
+const observatoryIntro = [
+  {
+    title: '천문대 NPC',
+    description: '안녕하세요. 이곳은 별자리를 관측할 수 있는 천문대입니다.',
+  },
+  {
+    title: '천문대 NPC',
+    description:
+      '스텔라리움을 통해 별자리 관측을 할 수 있으며, 원하는 지역을 선택해 그 지역의 밤하늘을 확인할 수 있어요.',
+  },
+  {
+    title: '천문대 NPC',
+    description: '그럼, 천문대에 입장하시겠습니까?',
+  },
+];
+
+export default observatoryIntro;
