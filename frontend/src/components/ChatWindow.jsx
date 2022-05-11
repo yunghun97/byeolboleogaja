407c5969918b94844450907ae811889ee66ff5d9
오용록 <ohyr9685@gmail.com>
2022-05-12 01:58:25 +0900

    Feat : update chat alone
    Former-commit-id: 2e6adc8c8f7f518a93b108cf1b16580ce34f577a

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useStore } from '@/store';
 import { Box, Divider, IconButton, InputBase, Paper } from '@mui/material';
 import {
   Send as SendIcon,
@@ -7,28 +8,102 @@ import {
 } from '@mui/icons-material';
 
 const ChatWindow = () => {
+  const msgBoxRef = useRef(null);
+  const nickname = useStore((state) => state.nickname);
+  const [messages, setMessages] = useState([]);
+  const [msg, setMsg] = useState('');
   const [open, setOpen] = useState(false);
 
-  const handleOpen = (e) => {
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages, open]);
+
+  const handleOpen = () => {
     setOpen(true);
   };
 
-  const handleClose = (e) => {
+  const handleClose = () => {
     setOpen(false);
   };
 
+  const scrollToBottom = () => {
+    if (msgBoxRef.current) {
+      msgBoxRef.current.scrollTop = msgBoxRef.current.scrollHeight;
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setMsg(e.target.value);
+  };
+
+  const handleSendMsg = (e) => {
+    //TODO: 소켓통신 send message
+    setMessages(messages.concat({ nickname, msg }));
+    setMsg('');
+  };
+
   return (
-    <Paper sx={{ opacity: '0.87', backgroundColor: '#777777' }}>
+    <Paper sx={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}>
       {open ? (
-        <Box sx={{ height: '15rem' }} />
+        <Box
+          ref={msgBoxRef}
+          sx={{
+            m: '0.5rem',
+            height: '20rem',
+            overflowY: 'auto',
+          }}
+        >
+          {messages.map((m, index) => (
+            <Box
+              key={index}
+              sx={{
+                mt: '0.25rem',
+                fontSize: '1rem',
+                fontWeight: 'bold',
+                color: '#ffffff',
+              }}
+            >{`${m.nickname}: ${m.msg}`}</Box>
+          ))}
+        </Box>
       ) : (
-        <Box sx={{ height: '2.5rem' }} />
+        <Box
+          sx={{
+            height: '2.5rem',
+            overflowY: 'auto',
+          }}
+        >
+          <Box
+            sx={{
+              m: '0.5rem',
+              fontSize: '1rem',
+              fontWeight: 'bold',
+              color: '#ffffff',
+            }}
+          >
+            {messages.length
+              ? `${messages.slice(-1)[0].nickname}: ${
+                  messages.slice(-1)[0].msg
+                }`
+              : ``}
+          </Box>
+        </Box>
       )}
       <Divider />
       <Paper
-        sx={{ height: '2.5rem', display: 'flex', backgroundColor: '#555555' }}
+        sx={{
+          height: '2.5rem',
+          display: 'flex',
+          backgroundColor: 'rgba(90, 90, 90, 0.87)',
+        }}
       >
         <InputBase
+          value={msg}
+          onChange={handleOnChange}
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              handleSendMsg(e);
+            }
+          }}
           sx={{
             fontSize: '1rem',
             fontWeight: 'bold',
@@ -37,7 +112,7 @@ const ChatWindow = () => {
             color: '#ffffff',
           }}
         />
-        <IconButton>
+        <IconButton onClick={handleSendMsg}>
           <SendIcon />
         </IconButton>
         {open ? (
