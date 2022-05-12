3452bdf7588591fe9086e3ff339c1b3a6e679719
박해인 <haein.hannah.park@gmail.com>
2022-05-12 15:01:31 +0900

    Feat: add loading scene on world container
    Former-commit-id: 4f8744abe7faf5d7e521a7afd247ea7144dedc77

--- frontend/src/components/WorldContainer.jsx
+++ frontend/src/components/WorldContainer.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'aframe';
 import { Button } from '@mui/material';
 import sky from '@/assets/img/world/bg-world.jpg?url';
@@ -17,14 +17,71 @@ import spaceshipNpc from '@/assets/model/world/mdl-npc-4.glb?url';
 import horoscopeNpc from '@/assets/model/world/mdl-npc-5.glb?url';
 import questionmark from '@/assets/img/world/img-questionmark.png';
 import GuideDialog from '@/components/GuideDialog';
+import LoadingScene from '@/components/LoadingScene';
 import { worldGuideInfos } from '@/constants';
 
 const WorldContainer = () => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const sceneEl = document.querySelector('a-scene');
+    const libraryEl = sceneEl.querySelector('#library-model');
+    const observatoryEl = sceneEl.querySelector('#observatory-model');
+    const spaceshipEl = sceneEl.querySelector('#spaceship-model');
+    const satelliteEl = sceneEl.querySelector('#satellite-model');
+    const witchHouseEl = sceneEl.querySelector('#witch-house-model');
+    const libraryNpcEl = sceneEl.querySelector('#libraryNpc-model');
+    const observatoryNpcEl = sceneEl.querySelector('#observatoryNpc-model');
+    const museumNpcEl = sceneEl.querySelector('#museumNpc-model');
+    const spaceshipNpcEl = sceneEl.querySelector('#spaceshipNpc-model');
+    const horoscopeNpcEl = sceneEl.querySelector('#horoscopeNpc-model');
+
+    libraryEl.addEventListener('model-loaded', function () {
+      setProgress(progress+1);
+    });
+    observatoryEl.addEventListener('model-loaded', function () {
+      setProgress(progress+1);
+    });
+    spaceshipEl.addEventListener('model-loaded', function () {
+      setProgress(progress+1);
+    });
+    satelliteEl.addEventListener('model-loaded', function () {
+      setProgress(progress+1);
+    });
+    witchHouseEl.addEventListener('model-loaded', function () {
+      setProgress(progress+1);
+    });
+    libraryNpcEl.addEventListener('model-loaded', function () {
+      setProgress(progress+1);
+    });
+    observatoryNpcEl.addEventListener('model-loaded', function () {
+      setProgress(progress+1);
+    });
+    museumNpcEl.addEventListener('model-loaded', function () {
+      setProgress(progress+1);
+    });
+    spaceshipNpcEl.addEventListener('model-loaded', function () {
+      setProgress(progress+1);
+    });
+    horoscopeNpcEl.addEventListener('model-loaded', function () {
+      setProgress(progress+1);
+    });
+  })
+
+  useEffect(() => {
+    // model-loaded 이벤트 3개가 1개로 처리되는 문제로 progress 값 6 이상으로 임의 설정
+    if (progress >= 6) {
+      setIsLoading(false);
+      setOpen(true);
+    }
+  },[progress])
 
   return (
     <>
-      <a-scene>
+      {isLoading && <LoadingScene loadingTime={progress}/>}
+      <a-scene loading-screen="enabled: false">
         <a-assets>
           <img id="sky" src={sky} />
           <a-asset-item id="ground" src={ground}></a-asset-item>
@@ -44,33 +101,39 @@ const WorldContainer = () => {
         <a-sky src="#sky" />
         <a-gltf-model src="#ground" position="0 -5 0" rotation="0 -2 0" />
         <a-gltf-model
+          id="library-model"
           src="#library"
           position="82.276 14.178 -204.775"
           rotation="0 90 0"
         />
         <a-gltf-model
+          id="observatory-model"
           src="#observatory"
           scale="120 120 120"
           position="41.511 29.726 -56.011"
           rotation="0 90 0"
         />
         <a-gltf-model
+          id="spaceship-model"
           src="#spaceship"
           scale="100 100 100"
           position="-60.211 -1.030 -262.203"
         />
         <a-gltf-model
+          id="museum-model"
           src="#museum"
           scale="2.3 2.3 2.3"
           position="-54.671 0 -86.823"
         />
         <a-gltf-model
+          id="satellite-model"
           src="#satellite"
           scale="100 100 100"
           position="-69.931 45.263 -98.965"
           animation="property: rotation; to: 0 360 0; loop: true; dur: 10000"
         />
         <a-gltf-model
+          id="witch-house-model"
           src="#witch-house"
           scale="0.3 0.3 0.3"
           position="-83.652 15.925 -359.571"
@@ -78,30 +141,35 @@ const WorldContainer = () => {
         />
 
         <a-gltf-model
+          id="libraryNpc-model"
           src="#libraryNpc"
           scale="12 12 12"
           position="50 2 -200"
           rotation="0 270 0"
         />
         <a-gltf-model
+          id="observatoryNpc-model"
           src="#observatoryNpc"
           scale="14 14 14"
           position="44 2 -60"
           rotation="0 270 0"
         />
         <a-gltf-model
+          id="museumNpc-model"
           src="#museumNpc"
           scale="15 15 15"
           position="-35 2.4 -70"
           rotation="0 30 0"
         />
         <a-gltf-model
+          id="spaceshipNpc-model"
           src="#spaceshipNpc"
           scale="15 15 15"
           position="-55 2.5 -200"
           rotation="0 60 0"
         />
         <a-gltf-model
+          id="horoscopeNpc-model"
           src="#horoscopeNpc"
           scale="15 15 15"
           position="-65 3 -345"
