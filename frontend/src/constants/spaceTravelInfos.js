cc3df588806196875aa52577650d657d450edc76
uiyn <uiyn@naver.com>
2022-05-13 11:31:50 +0900

    Feat : add spaceTravelInfos id
    Former-commit-id: 8a8087ee7fba48f16a23645392c47a8b7a9a18bc

--- frontend/src/constants/spaceTravelInfos.js
+++ frontend/src/constants/spaceTravelInfos.js
@@ -4,22 +4,23 @@ import Sun from '@/assets/img/spaceship/img-sun.jpg';
 
 const spaceTravelInfos = [
   {
+    id: 'moon',
     title: '달',
     description: '약 38만 5천 km',
     imgPath: Moon,
   },
   {
+    id: 'mars',
     title: '화성',
-    description:
-      '약 225만 km',
+    description: '약 225만 km',
     imgPath: Mars,
   },
   {
+    id: 'sun',
     title: '태양',
-    description:
-      '약 1억 4959만 km',
+    description: '약 1억 4959만 km',
     imgPath: Sun,
-  }
+  },
 ];
 
 export default spaceTravelInfos;
