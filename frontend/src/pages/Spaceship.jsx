883d5dbea1f0b0c6a5580d4d63c4a68fa8d545c5
uiyn <uiyn@naver.com>
2022-05-13 10:41:06 +0900

    Feat : add moon sky & ground asset
    Former-commit-id: 445f62b62769785de923e94102f41546d737ed64

---
+++ frontend/src/pages/Spaceship.jsx
@@ -0,0 +1,11 @@
+import SpaceTravelDialog from '@/components/SpaceTravelDialog';
+
+export default function Spaceship() {
+  return (
+    <>
+      <main style={{ padding: '1rem 0' }}>
+        <SpaceTravelDialog />
+      </main>
+    </>
+  );
+}
