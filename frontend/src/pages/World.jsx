446f2a7d29973ce4a50ed3f4e7163188e8e4f72e
윤숙 <dbstnr2146@naver.com>
2022-05-19 01:35:57 +0900

    Feat : add menu-ment
    Former-commit-id: 9559a711d42f243d1bdd1e745680edfc26bb8f51

--- frontend/src/pages/World.jsx
+++ frontend/src/pages/World.jsx
@@ -4,26 +4,63 @@ import WorldGuideDialog from '@/components/WorldGuideDialog';
 import Menu from '@/components/Menu';
 import WorldContainer from '@/components/WorldContainer';
 import { worldGuideInfos } from '@/constants';
-
+import { Snackbar, IconButton } from '@mui/material';
+import { Close } from '@mui/icons-material';
 export default function World() {
   const [guideOpen, setGuideOpen] = useState(false);
   const [open, setOpen] = useState(false);
+  const [snackOpenCheck, setSnackOpenCheck] = useState(false);
+  const [snackOpen, setSnackOpen] = useState(false);
   const isSkip = useStore((state) => state.isSkip);
-
+  const nickname = useStore((state) => state.nickname);
   useEffect(() => {
     if (isSkip == 0) {
       setTimeout(function () {
         setOpen(true);
+
+        setSnackOpen(true);
       }, 3000);
     }
     if (isSkip === 1) {
+      setSnackOpenCheck(true);
       setOpen(false);
     }
   }, []);
+  useEffect(() => {
+    if (snackOpenCheck == true) {
+      setSnackOpen(false);
+    }
+  });
+  const handleClose = () => {
+    setSnackOpen(false);
+  };
+  const action = (
+    <>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <Close fontSize="small" />
+      </IconButton>
+    </>
+  );
 
   return (
     <main>
       <WorldContainer />
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        open={snackOpen}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        message={`${nickname}` + ' 어서와~  우리 별보러 함께 모험을 떠나자!'}
+        action={action}
+      />
       <WorldGuideDialog
         guideInfos={worldGuideInfos}
         open={open}
