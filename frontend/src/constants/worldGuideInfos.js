234be00a00d426e7ebb344892ffa652d1cb1d777
박해인 <haein.hannah.park@gmail.com>
2022-05-18 10:23:48 +0900

    refactor: edited world guide info description
    Former-commit-id: 31536dd9f862e5fce41f0c90ac873dd948677de0

--- frontend/src/constants/worldGuideInfos.js
+++ frontend/src/constants/worldGuideInfos.js
@@ -9,35 +9,35 @@ import guidecharacter from '@/assets/img/observatory/img-guidecharacter.png';
 const worldGuideInfos = [
   {
     title: '방향키 이동',
-    description: '상하좌우 방향키와 wasd키로 월드맵을 이동할 수 있고, 마우스로 시점을 전환할 수 있어요.',
+    description: '상하좌우 방향키와 wasd키로 월드맵을 이동할 수 있고, 마우스를 드래그해서 시점을 전환할 수 있어요.',
     imgPath: movement,
   },
   {
     title: '천문대',
     description:
-      '천문대에 진입하면 현재 위치의 하늘에 떠있는 별을 관측하거나 오로라를 볼 수 있어요.',
+      '천문대 NPC에게 말을 걸면 현재 위치의 하늘에 떠있는 별을 관측하거나 오로라를 볼 수 있어요.',
     imgPath: observatory,
   },
   {
     title: '도서관',
     description:
-      '도서관에 진입하면 별자리 이야기, 우주에 관한 상식, 우주비행사 이야기, 행성 이야기를 열람할 수 있어요.',
+      '도서관 NPC에게 말을 걸면 별자리 이야기, 우주에 관한 상식, 우주비행사 이야기, 행성 이야기를 열람할 수 있어요.',
     imgPath: library,
   },
   {
     title: '우주 박물관',
-    description: '우주 박물관에 진입하면 1층에서는 인공위성 3D 모델을 관람할 수 있고, 2층에서는 허블 망원경으로 촬영된 우주 사진을 관람할 수 있어요.',
+    description: '우주 박물관 NPC에게 말을 걸면 1층에서는 인공위성 3D 모델을 관람할 수 있고, 2층에서는 허블 망원경으로 촬영된 우주 사진을 관람할 수 있어요.',
     imgPath: museum,
   },
   {
     title: '우주선',
     description:
-      '우주선을 타고 달로 이동하여 토끼를 만나 퀴즈를 풀어볼 수 있어요.',
+      '우주선 NPC에게 말을 걸면 우주선을 타고 달로 이동하여 토끼를 만나 퀴즈를 풀어볼 수 있어요.',
     imgPath: spaceship,
   },
   {
     title: '오늘의 운세',
-    description: '자신의 별자리에 따른 오늘의 운세를 확인할 수 있어요.',
+    description: '운세 NPC에게 말을 걸면 자신의 별자리에 따른 오늘의 운세를 확인할 수 있어요.',
     imgPath: astrology,
   },
   {
