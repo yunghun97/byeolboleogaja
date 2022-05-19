14f5b30dc05491baddc944d3232b9d77bf014829
오용록 <ohyr9685@gmail.com>
2022-05-19 15:36:18 +0900

    Feat : loading
    Former-commit-id: e3d9a804e2193ee39de93a745eb46590864278bb

--- frontend/src/pages/World.jsx
+++ frontend/src/pages/World.jsx
@@ -3,6 +3,7 @@ import { useStore } from '@/store';
 import WorldGuideDialog from '@/components/WorldGuideDialog';
 import Menu from '@/components/Menu';
 import WorldContainer from '@/components/WorldContainer';
+import LoadingScene from '@/components/LoadingScene';
 import { worldGuideInfos } from '@/constants';
 import { Snackbar, IconButton } from '@mui/material';
 import { Close } from '@mui/icons-material';
@@ -13,10 +14,16 @@ export default function World() {
   const isSkip = useStore((state) => state.isSkip);
   const nickname = useStore((state) => state.nickname);
 
+  const LOADING_TIME = 2000;
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     if (isSkip === 0) {
       setOpen(true);
       setSnackOpen(true);
+    }
+    if (isLoading) {
+      setTimeout(() => setIsLoading(false), LOADING_TIME + 1000);
     }
   }, []);
 
@@ -39,29 +46,35 @@ export default function World() {
 
   return (
     <>
-      <WorldContainer />
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        open={snackOpen}
-        autoHideDuration={3000}
-        onClose={handleClose}
-        message={`${nickname} 님! 별보러가자와 함께 즐거운 우주 여행되세요!`}
-        action={action}
-      />
-      <WorldGuideDialog
-        guideInfos={worldGuideInfos}
-        open={open}
-        setOpen={setOpen}
-      />
-      <Menu
-        isGuideDialog={true}
-        setGuideOpen={setOpen}
-        isWorld={false}
-        placeBGM={'world'}
-      />
+      {isLoading ? (
+        <LoadingScene loadingTime={LOADING_TIME} />
+      ) : (
+        <>
+          <WorldContainer />
+          <Snackbar
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            open={snackOpen}
+            autoHideDuration={3000}
+            onClose={handleClose}
+            message={`${nickname} 님! 별보러가자와 함께 즐거운 우주 여행되세요!`}
+            action={action}
+          />
+          <WorldGuideDialog
+            guideInfos={worldGuideInfos}
+            open={open}
+            setOpen={setOpen}
+          />
+          <Menu
+            isGuideDialog={true}
+            setGuideOpen={setOpen}
+            isWorld={false}
+            placeBGM={'world'}
+          />
+        </>
+      )}
     </>
   );
 }
