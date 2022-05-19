f99ec6e7a0ec032c5def7b0fa297e5525e50a7ab
오용록 <ohyr9685@gmail.com>
2022-05-19 23:13:39 +0900

    Feat : add spaceship in moon
    Former-commit-id: 94dedde25fa29b107c5e7ea422a3eb8503203e54

--- frontend/src/components/MoonContainer.jsx
+++ frontend/src/components/MoonContainer.jsx
@@ -1,23 +1,29 @@
 import 'aframe';
 import ground from '@/assets/model/moon/mdl-moon-ground.glb?url';
 import sky from '@/assets/img/moon/bg-moonsky.jpg?url';
-
 import flag from '@/assets/model/moon/mdl-flag.glb?url';
 import rabbit from '@/assets/model/moon/mdl-rabbit.glb?url';
+import spaceship from '@/assets/model/world/mdl-spaceship.glb?url';
+import spaceshipNpc from '@/assets/model/world/mdl-npc-4.glb?url';
 import PlanetDialog from '@/components/PlanetDialog';
-import { moonInfos } from '@/constants';
+import QuizDialog from '@/components/QuizDialog';
+import BuildingDialog from '@/components/BuildingDialog';
+import { moonInfos, goHomeInfos } from '@/constants';
 import { useState, useEffect } from 'react';
 import { useStore } from '@/store';
-import QuizDialog from './QuizDialog';
 
 const MoonContainer = () => {
   const [open, setOpen] = useState(false);
   const [quizOpen, setQuizOpen] = useState(false);
+  const [buildingOpen, setBuildingOpen] = useState(false);
+  const [info, setInfo] = useState(moonInfos);
+  const [building, setBuilding] = useState('');
   const chracterColor = useStore((state) => state.chracterColor);
 
   useEffect(() => {
     const sceneEl = document.querySelector('a-scene');
     const rabbitNpcEl = sceneEl.querySelector('#rabbitNpc');
+    const spaceshipNpcEl = sceneEl.querySelector('#spaceshipNpc-model');
     const player = sceneEl.querySelector('#player');
 
     document.addEventListener('keydown', function (event) {
@@ -48,6 +54,13 @@ const MoonContainer = () => {
 
     rabbitNpcEl.addEventListener('click', function () {
       setOpen(true);
+      setInfo(moonInfos);
+    });
+
+    spaceshipNpcEl.addEventListener('click', function () {
+      setBuildingOpen(true);
+      setInfo(goHomeInfos);
+      setBuilding('world');
     });
   });
 
@@ -60,6 +73,8 @@ const MoonContainer = () => {
           <a-asset-item id="amongus" src={chracterColor}></a-asset-item>
           <a-asset-item id="flag" src={flag}></a-asset-item>
           <a-asset-item id="rabbit" src={rabbit}></a-asset-item>
+          <a-asset-item id="spaceship" src={spaceship}></a-asset-item>
+          <a-asset-item id="spaceshipNpc" src={spaceshipNpc}></a-asset-item>
         </a-assets>
         <a-sky src="#sky" />
         <a-gltf-model src="#ground" position="0 -5 0" rotation="0 -2 0" />
@@ -71,6 +86,24 @@ const MoonContainer = () => {
           position="0 -7 -25"
           scale="1 1 1"
           animation-mixer="clip : Take 001"
+        />
+
+        <a-gltf-model
+          id="spaceship-model"
+          src="#spaceship"
+          shadow="cast: true; receive: false"
+          scale="130 130 130"
+          position="80.37 -5.8 53"
+          rotation="0 140 0"
+        />
+        <a-gltf-model
+          id="spaceshipNpc-model"
+          class="clickable"
+          src="#spaceshipNpc"
+          shadow="cast: true; receive: false"
+          scale="23 23 23"
+          position="100 1 10.8"
+          rotation="0 260 0"
         />
 
         <a-camera position="0 7 0">
@@ -88,12 +121,18 @@ const MoonContainer = () => {
         </a-camera>
       </a-scene>
       <PlanetDialog
-        planetInfos={moonInfos}
+        planetInfos={info}
         open={open}
         setOpen={setOpen}
         setQuizOpen={setQuizOpen}
       />
       <QuizDialog open={quizOpen} setOpen={setQuizOpen} />
+      <BuildingDialog
+        buildingInfos={info}
+        building={building}
+        open={buildingOpen}
+        setOpen={setBuildingOpen}
+      />
     </div>
   );
 };
