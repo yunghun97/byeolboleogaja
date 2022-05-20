2bad69f434a674af382d6cbc0339de4f09f35730
오용록 <ohyr9685@gmail.com>
2022-05-20 10:38:47 +0900

    Feat : add get apod in s3
    Former-commit-id: 2f5f109922f889fed2544df9d79e7b878d18cd23

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -4,6 +4,7 @@ import MainBackground from '@/components/MainBackground';
 import MainContainer from '@/components/MainContainer';
 import Footer from '@/components/Footer';
 import { getApod } from '@/api/nasa';
+import { getApodS3 } from '@/api/apod';
 import andromedaBg from '@/assets/img/loading/bg-loading-1.jpg';
 
 export default function MainPage() {
@@ -21,8 +22,24 @@ export default function MainPage() {
       $setApodUrl(res.data.url);
     };
     img.onerror = () => {
-      $setApodUrl(andromedaBg);
+      initApodUrlS3();
     };
+  };
+
+  const initApodUrlS3 = async () => {
+    try {
+      const res = await getApodS3();
+      const img = new Image();
+      img.src = res.data.url;
+      img.onload = () => {
+        $setApodUrl(res.data.url);
+      };
+      img.onerror = () => {
+        $setApodUrl(andromedaBg);
+      };
+    } catch (error) {
+      $setApodUrl(andromedaBg);
+    }
   };
 
   return (
